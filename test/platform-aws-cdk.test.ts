dfbff05e641457fed2f695d0ebfdcecf32e7def9
Jay <jayykhan0713@gmail.com>
2026-01-16 12:45:44 -0800

    cdk typescript project initialized

---
+++ test/platform-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PlatformAwsCdk from '../lib/platform-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/platform-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PlatformAwsCdk.PlatformAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
