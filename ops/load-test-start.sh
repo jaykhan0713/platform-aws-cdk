f37b47262cdd6a658b164fb2268010085fb33276
jaykhan0713 <jaykhan0713@gmail.com>
2026-02-26 13:43:39 -0800

    added lambda load-test start script

---
+++ ops/load-test-start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+aws lambda invoke \
+  --function-name jay-platform-k6-runner-lambda-prod \
+  --cli-binary-format raw-in-base64-out \
+  --payload '{"vus":4,"duration":"10m","sleepIntervalMs":0.03}' \
+  response.json
+
+cat response.json
+rm -f response.json
+
+echo
+read -p "Done. Press enter to close..."
