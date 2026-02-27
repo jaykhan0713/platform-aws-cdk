fdd1e592104a76b045482d33d5c3ddf97a0480ce
jaykhan0713 <jaykhan0713@gmail.com>
2026-02-26 17:25:54 -0800

    increased loadtest to 20m

--- ops/load-test-start.sh
+++ ops/load-test-start.sh
@@ -3,7 +3,7 @@
 aws lambda invoke \
   --function-name jay-platform-k6-runner-lambda-prod \
   --cli-binary-format raw-in-base64-out \
-  --payload '{"vus":4,"duration":"10m","sleepIntervalMs":0.03}' \
+  --payload '{"vus":4,"duration":"20m","sleepIntervalMs":0.03}' \
   response.json
 
 cat response.json
