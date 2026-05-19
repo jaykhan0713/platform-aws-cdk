fbfd5aecdb7e0f9d922a45894df7b67c67466cbb
Jay Khan <jaykhan0713@gmail.com>
2026-05-19 11:39:48 -0700

    load test script formatting

--- ops/load-test/gotenberg.sh
+++ ops/load-test/gotenberg.sh
@@ -3,7 +3,12 @@
 aws lambda invoke \
   --function-name jay-platform-k6-runner-lambda-prod \
   --cli-binary-format raw-in-base64-out \
-  --payload '{"vus":1,"duration":"10m","sleepIntervalMs":0.05,"gotenberg"}' \
+  --payload '{
+    "vus": 1,
+    "duration": "10m",
+    "sleepIntervalMs": 0.05,
+    "gotenberg": true
+  }' \
   response.json
 
 cat response.json
